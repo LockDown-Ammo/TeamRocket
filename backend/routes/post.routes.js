e1dd22f799b585300aba5eea5cd61b2e28ef918f
Arnav Gupta <ganurag1973@gmail.com>
2026-02-22 02:49:43 +0530

    Backend: Post Reporting

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -5,5 +5,6 @@ const authMiddleware = require('../middleware/auth.middleware')
 
 router.post('/', authMiddleware, postController.createPost)
 router.post('/:id/rate', authMiddleware, postController.ratePost)
+router.post('/:id/report', authMiddleware, postController.reportPost)
 
 module.exports = router
