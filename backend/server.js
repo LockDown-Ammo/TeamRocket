6df7b6647de3779396632f6a3884a0dea4bedd09
Arnav Gupta <ganurag1973@gmail.com>
2026-02-22 00:27:43 +0530

    Fixed typo

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
+app.use('/api/post', require("./routes/post.routes"))
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log("MonoDB Connected");
+    console.log("MongoDB Connected");
 
     app.listen(process.env.PORT, () => {
       console.log(`Server running on port ${process.env.PORT}`);
