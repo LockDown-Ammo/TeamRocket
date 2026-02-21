89defc0b13a7a2290a93e2af4268d364591ac783
Arnav Gupta <ganurag1973@gmail.com>
2026-02-22 00:27:52 +0530

    Removed unused import

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,4 @@
 const User = require("../models/User");
-const express = require("express");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
